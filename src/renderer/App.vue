4fde859787b2b94da112ac4209d357b8d78bdd73
wcadena <wcadena@outlook.com>
2019-10-28 12:05:36 -0500

    Se actualiza con Javascript

---
+++ src/renderer/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div v-if="isLoading">
+      <router-view></router-view>
+    </div>
+    <div v-else>
+      <v-app>
+        <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
+      </v-app>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoading: false
+    };
+  },
+  async created() {
+    await this.$store.dispatch("GET_DATA");
+    this.isLoading = true;
+  }
+};
+</script>
+
+<style>
+/* CSS */
+</style>
---
+++ src/renderer/pages/notfound.vue
@@ -0,0 +1,19 @@
+<template>
+  <default-layout>
+    <v-btn @click="$router.push('')"></v-btn>
+    <v-btn @click="w"></v-btn>
+  </default-layout>
+</template>
+
+<script>
+export default {
+  methods: {
+    w() {
+      console.log(this.$route);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
